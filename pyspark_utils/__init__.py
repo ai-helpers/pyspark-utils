62db218a7986dddabcc23b714bef317202edef21
data-corentinv <corentin.vasseur@decathlon.com>
2024-05-30 09:48:43 +0200

    fix: pkg_resources deprecated

--- pyspark_utils/__init__.py
+++ pyspark_utils/__init__.py
@@ -1,11 +1,3 @@
-from pkg_resources import DistributionNotFound, get_distribution
 from .utils import get_spark_session, assert_cols_in_df, assert_df_close, with_columns  # noqa: F401
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "pyspark-utils"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+__version__ = "0.1.0a1"
