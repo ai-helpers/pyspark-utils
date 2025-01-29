d7a8f918b7b18a49c23118e05edfd2d7cdaaaf4c
data-corentinv <corentin.vasseur@decathlon.com>
2025-01-29 14:41:04 +0000

    Deploying to gh-pages from @ ai-helpers/pyspark-utils@ddee8186bfeff04f2c143545f5994eeb9f56cb80 🚀

--- _static/conf.py
+++ _static/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, AI-Helpers"
 author = "AI-Helpers Team"
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.0a3"
+version = "0.1.0a4"
 release = version
 
 # -- General configuration ---------------------------------------------------
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0a3',
+    VERSION: '0.1.0a4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
